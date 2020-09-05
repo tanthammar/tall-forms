931862bdbabd5b92a77efb6784f122976163d738
tanthammar <tinahammar@gmail.com>
2020-09-05 12:02:03 +0200

    added cols to config

--- config/tall-forms.php
+++ config/tall-forms.php
@@ -61,6 +61,12 @@ return [
         '4' => 'sm:col-span-4',
         '5' => 'sm:col-span-5',
         '6' => 'sm:col-span-6',
+        '7' => 'sm:col-span-7',
+        '8' => 'sm:col-span-8',
+        '9' => 'sm:col-span-9',
+        '10' => 'sm:col-span-10',
+        '11' => 'sm:col-span-11',
+        '12' => 'sm:col-span-12',
     ],
 
     //File upload default validation message
