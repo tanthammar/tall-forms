6693bf84b5846d18d6e6af7137677eecd677e3b9
tanthammar <tinahammar@gmail.com>
2020-09-24 17:58:59 +0200

    move save message closer to save and stay

--- resources/views/includes/buttons-root.blade.php
+++ resources/views/includes/buttons-root.blade.php
@@ -6,14 +6,14 @@
         @if($showReset)
             <x-tall-button type="reset" wire:click.prevent="resetFormData" :text="trans(config('tall-forms.reset'))" color="warning"/>
         @endif
-        <span x-data="{ open: false }"
-              x-init="@this.on('notify-saved', () => { if (open === false) setTimeout(() => { open = false }, 2500); open = true;})"
-              x-show.transition.out.duration.1000ms="open" style="display: none;"
-              class="text-gray-500">{{ trans(config('tall-forms.saved')) }}</span>
         @if($showGoBack)
             <x-tall-button wire:click.prevent="saveAndGoBack"
                       color="primary">@lang(config('tall-forms.save-go-back'))</x-tall-button>
         @endif
+            <span x-data="{ open: false }"
+                  x-init="@this.on('notify-saved', () => { if (open === false) setTimeout(() => { open = false }, 2500); open = true;})"
+                  x-show.transition.out.duration.1000ms="open" style="display: none;"
+                  class="text-gray-500">{{ trans(config('tall-forms.saved')) }}</span>
         <x-tall-button type="submit" wire:click.prevent="saveAndStay" wire:loading.attr="disabled" color="positive">
                     <span class="mr-2" wire:loading wire:target="saveAndStay">
                         <x-tall-spinner/></span>
