e7b7afb4e5c6ff7dc35aea2937a9cc17564a4aff
tanthammar <tinahammar@gmail.com>
2020-10-16 17:35:04 +0200

    fixed merged file name

---
+++ resources/views/fields/search.blade.php
@@ -0,0 +1,13 @@
+<div x-data="{
+    optionsVisible: false,
+    field: @entangle($temp_key),
+    selected: null,
+    searchInput: @entangle($field->searchKey)}
+">
+    <x-tall-search
+        :field="$field"
+        :temp-key="$temp_key"
+        :options="${$field->optionsKey}"
+    />
+</div>
+
