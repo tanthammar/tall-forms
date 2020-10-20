6d9d2287affaa514253faa1044a3b0e175def662
tanthammar <tinahammar@gmail.com>
2020-10-20 16:46:33 +0200

    css ImageCropper

--- src/Components/ImageCropper.php
+++ src/Components/ImageCropper.php
@@ -13,10 +13,10 @@ class ImageCropper extends Component
     public string $temp_key;
     public string $imageUrl;
 
-    public string $root = 'w-full p-4 mx-auto border-gray-300 border bg-white rounded relative';
+    public string $root = 'bg-white border-2 border-dashed border-gray-300 mx-auto p-4 relative rounded w-full';
     public string $wrapper = 'active:border-blue-500';
-    public string $dropZone = 'cursor-pointer text-center uppercase text-sm text-bold text-gray-700';
-    public string $fileInfo = 'text-xs text-gray-600';
+    public string $dropZone = 'cursor-pointer font-bold text-center text-gray-400 text-xs tracking-wide uppercase';
+    public string $fileInfo = 'font-hairline text-gray-500 text-xs';
     public string $icon = 'mx-auto h-12 w-12 text-gray-400';
     public string $modalbg = 'fixed inset-0 h-screen w-screen p-2 sm:p-10 md:p-20 bg-gray-800 bg-opacity-75 z-50';
     public string $modal = 'bg-white rounded w-full h-full flex justify-center items-center';
@@ -25,7 +25,7 @@ class ImageCropper extends Component
     public string $editBtnWrapper = 'z-10 absolute inset-0';
     public string $thumbnail = 'w-full h-full';
     //buttons
-    public string $upload = 'flex items-center mx-auto mt-2 px-2 text-white text-sm text-center font-medium border border-transparent rounded outline-none bg-gray-500';
+    public string $upload = 'bg-gray-400 flex font-medium items-center mt-2 mx-auto outline-none px-2 rounded-sm text-center text-white text-xs';
     public string $delete = 'bg-red-500 text-white p-2 rounded';
     public string $save = 'bg-teal-500 text-white p-2 rounded';
     public string $swap = 'bg-red-500 bg-opacity-75 hover:bg-opacity-100 text-white p-2 rounded';
