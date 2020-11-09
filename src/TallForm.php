b01262c3bb79df6f5cb2f6924f4aefba79537f4f
tanthammar <tinahammar@gmail.com>
2020-11-09 18:22:06 +0100

    if model is null

--- src/TallForm.php
+++ src/TallForm.php
@@ -56,7 +56,7 @@ trait TallForm
 
     public function setFormProperties()
     {
-        $this->form_data = $this->model->only($this->firstLevelfieldNames());
+        $this->form_data = optional($this->model)->only($this->firstLevelfieldNames()) ?? [];
 
         // foreach ($this->getFields(false) as $field) {
         //     if (filled($field) && !isset($this->form_data[$field->name])) {
