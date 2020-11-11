3872e87e2592af1458534436eebd5932f0f8bea4
tanthammar <tinahammar@gmail.com>
2020-11-11 13:48:55 +0100

    remove deprecated v3 methods from submit()

--- src/Traits/SubmitsForm.php
+++ src/Traits/SubmitsForm.php
@@ -13,37 +13,22 @@ trait SubmitsForm
     {
         $validated_data = $this->validate($this->get_rules())['form_data'];
         $field_names = [];
-        $relationship_names = [];
         $custom_names = [];
 
         foreach ($this->fields() as $field) {
             if (filled($field)) {
-                if ($field->is_relation) {
-                    $relationship_names[] = $field->name;
-                } elseif ($field->is_custom) {
-                    $custom_names[] = $field->name;
-                } else {
-                    $field_names[] = $field->name;
-                }
+                if ($field->is_custom) $custom_names[] = $field->name;
+                if (!$field->is_relation && !$field->is_custom) $field_names[] = $field->name;
             }
         }
 
-        $relationship_data = Arr::only($validated_data, $relationship_names);
         $this->custom_data = Arr::only($validated_data, $custom_names); //custom_data also used by syncTags(), therefore must be a property
         $model_fields_data = Arr::only($validated_data, $field_names);
 
         //make sure to create the model before attaching any relations
         $this->success($model_fields_data); //creates or updates the model
 
-        //save relations, group method, legacy method from v3
-        if (optional($this->model)->exists) {
-            $this->relations($relationship_data);
-        }
-
-        //save custom fields, group method, legacy method from v3
-        $this->custom_fields($this->custom_data);
-
-        //saveFoo() v4 method
+        //saveFoo()
         foreach ($this->fields() as $field) {
             if (filled($field)) {
                 $function = $this->parseFunctionNameFrom($field->key, 'save');
