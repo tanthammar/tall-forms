c6cab34b4ae0bef86c4fc0fea60a43dd07485a20
tanthammar <tinahammar@gmail.com>
2020-11-18 12:52:09 +0100

    New: TallFormComponent, Optional class to extend, if you have Trait method naming, collisions

---
+++ src/TallFormComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace Tanthammar\TallForms;
+
+/**
+ * Optional class to extend, if you have Trait method naming, collisions
+ * @package App\Http\Livewire
+ */
+class TallFormComponent extends \Livewire\Component
+{
+    use TallForm;
+}
