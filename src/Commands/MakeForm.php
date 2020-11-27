fea680c53a2de8c341c2c0d2f8698b2c42361b5b
tanthammar <tinahammar@gmail.com>
2020-11-27 14:00:44 +0100

    Updated make command, added option to set full path. (namespace)

--- src/Commands/MakeForm.php
+++ src/Commands/MakeForm.php
@@ -8,27 +8,34 @@ use Illuminate\Support\Str;
 
 class MakeForm extends Command
 {
-    protected $signature = 'make:tall-form {name} {--model=Model} {--path=Forms} {--modelspath=Models\} {--action=create}';
+    protected $signature = 'make:tall-form {name} {--model=Model} {--path=Http/Livewire/Forms} {--modelspath=Models/} {--action=create}';
     protected $description = 'Make a new Laravel Livewire form component.';
 
     public function handle()
     {
-        $stub = $this->option('action') == 'create' ?  File::get(__DIR__ . '/../../resources/stubs/create-component.stub') : File::get(__DIR__ . '/../../resources/stubs/update-component.stub');
+        $stub = $this->option('action') == 'create' ? File::get(__DIR__ . '/../../resources/stubs/create-component.stub') : File::get(__DIR__ . '/../../resources/stubs/update-component.stub');
         $stub = str_replace('FormTitle', config('tall-forms.form-title'), $stub);
         $stub = str_replace('DummyComponent', $this->argument('name'), $stub);
         $stub = str_replace('DummyModel', $this->option('model'), $stub);
         $stub = str_replace('dummymodel', Str::lower($this->option('model')), $stub);
-        $stub = str_replace('ModelsPath', $this->option('modelspath'), $stub);
+
+        $modelspath = Str::of($this->option('modelspath'))->replace('/', "\\");
+        $stub = str_replace('ModelsPath', $modelspath, $stub);
+
         //$stub = str_replace('Action', $this->option('action'), $stub);
         //$stub = str_replace('DummyRoute', Str::slug(Str::plural($this->option('model'))), $stub);
-        $stub = str_replace('Namespace', Str::of($this->option('path'))->replace('/', "\\"), $stub);
-        $path = app_path('Http/Livewire/'. $this->option('path') . "/" . $this->argument('name') . '.php');
+        $namespace = Str::of($this->option('path'))->replace('/', "\\");
+        $stub = str_replace('Namespace', $namespace, $stub);
 
-        File::ensureDirectoryExists(app_path('Http/Livewire/') . $this->option('path') . "/");
+        $path = Str::of($this->option('path'))->replace('\\', "/");
+        $path .= "/";
+        $file_name = app_path($path . $this->argument('name') . '.php');
 
-        if (!File::exists($path) || $this->confirm($this->argument('name') . ' already exists. Overwrite it?')) {
-            File::put($path, $stub);
-            $this->info('Http/Livewire/' . $this->option('path') . "/" . $this->argument('name')  . ' was made!');
+        if (!is_dir(app_path($path))) File::makeDirectory(app_path($path), 0755, true);
+
+        if (!File::exists($file_name) || $this->confirm($this->argument('name') . ' already exists. Overwrite it?')) {
+            File::put($file_name, $stub);
+            $this->info('App/' . $path . $this->argument('name') . ' was made!');
         }
     }
 }
