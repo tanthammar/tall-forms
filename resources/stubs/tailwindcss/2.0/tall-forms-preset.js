a92f1dc70fef27fbe6c58b6c887879fea9d123d8
tanthammar <tinahammar@gmail.com>
2020-12-07 23:25:00 +0100

    2.0 preset error

--- resources/stubs/tailwindcss/2.0/tall-forms-preset.js
+++ resources/stubs/tailwindcss/2.0/tall-forms-preset.js
@@ -41,6 +41,6 @@ module.exports = {
     plugins: [
         require('@tailwindcss/typography'),
         require('@tailwindcss/forms'),
-        require('@tailwindcss/aspect-ratio)',
+        require('@tailwindcss/aspect-ratio'),
     ],
 };
