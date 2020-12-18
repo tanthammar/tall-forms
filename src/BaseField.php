993d26df7e6cc6c83a76760e93da49d31eba45cd
tanthammar <tinahammar@gmail.com>
2020-12-18 17:16:43 +0100

    added $ignored for future use with design elements

--- src/BaseField.php
+++ src/BaseField.php
@@ -36,6 +36,9 @@ class BaseField
     public $allowed_in_keyval = true;
     public $allowed_in_tab = true;
 
+    //Tabs, Groups, Panels and similar design elements that has a field array but should be ignored in form_data and validation
+    public bool $ignored = false;
+
     public function __construct($label, $key)
     {
         $this->label = $label;
