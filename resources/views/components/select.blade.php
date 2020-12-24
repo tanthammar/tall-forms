6873a3c4a0bd2a6ae4454b5e8102f380663e1e42
Mert Aşan <mertemreasan@gmail.com>
2020-12-24 23:41:38 +0300

    added $error() and $class() methods

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -1,4 +1,6 @@
-<select @foreach($options() as $key => $value) {{$key}}="{{$value}}" @endforeach>
+<select @foreach($options() as $key => $value) {{$key}}="{{$value}}" @endforeach
+{{ $attributes->merge(['class' => $errors->has($field->key) ? $error() : $class() ]) }}
+>
 @if($field->placeholder) <option value="">{{ $field->placeholder }}</option> @endif
 @forelse($field->options as $value => $label)
 <option wire:key="{{ md5($field->key.$value) }}" value="{{ $value }}">{{ $label }}</option>
