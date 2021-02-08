5e61c3a97a2b0a634363164fc8bd2bdabfe0c522
tanthammar <tinahammar@gmail.com>
2021-02-08 14:11:01 +0100

    TallFormComponent should be abstract class

--- src/TallFormComponent.php
+++ src/TallFormComponent.php
@@ -7,7 +7,7 @@ namespace Tanthammar\TallForms;
  * Optional class to extend, if you have Trait method naming, collisions
  * @package App\Http\Livewire
  */
-class TallFormComponent extends \Livewire\Component
+abstract class TallFormComponent extends \Livewire\Component
 {
     use TallForm;
 }
