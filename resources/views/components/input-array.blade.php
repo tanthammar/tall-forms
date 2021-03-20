59a26ddd88dd7db3ed5016f0eee85db2806c2a3a
tanthammar <tinahammar@gmail.com>
2021-03-20 17:17:46 +0100

    focus last input on enter and delete input item

--- resources/views/components/input-array.blade.php
+++ resources/views/components/input-array.blade.php
@@ -3,12 +3,19 @@
         addItem() {
             this.inputArray = Array.from(this.inputArray.filter(item => item.length > 0))
             this.inputArray.push('')
+            this.focusLastInput()
         },
         deleteItem(index) {
             this.inputArray.splice(index, 1)
+            this.focusLastInput()
+        },
+        focusLastInput() {
+            this.$nextTick(() => {
+                this.$refs.inputs.lastElementChild.firstElementChild.focus()
+            })
         }
     }">
-    <div @error($field->key.'.*') class="{{ $error() }}" @enderror>
+    <div x-ref="inputs" @error($field->key.'.*') class="{{ $error() }}" @enderror>
         <template x-for="(item, index) in inputArray" :key="index">
             <div class="flex md:space-x-2 space-x-1">
                 <input
@@ -16,7 +23,7 @@
                     x-on:keydown.enter.prevent="addItem()"
                     @foreach($options() as $key => $value) {{$key}}="{{$value}}" @endforeach
                 />
-                <button type="button" class="tf-repeater-delete-btn" x-on:click.prevent.prevent="deleteItem(index)">
+                <button type="button" class="tf-repeater-delete-btn" x-on:click.prevent.prevent="deleteItem(index)" tabindex="-1">
                     <x-tall-svg :path="config('tall-forms.trash-icon')" class="tf-repeater-btn-size" />
                 </button>
             </div>
