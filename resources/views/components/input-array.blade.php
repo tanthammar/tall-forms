adcb5af58d74cb33cfc04173b79c767f0e6d9c07
tanthammar <tinahammar@gmail.com>
2021-03-20 15:35:35 +0100

    tidy code

--- resources/views/components/input-array.blade.php
+++ resources/views/components/input-array.blade.php
@@ -1,11 +1,22 @@
-<div x-data="{ inputArray: @entangle($field->key){{$field->defer}} }">
+<div x-data="{
+        inputArray: @entangle($field->key){{$field->defer}},
+        addItem() {
+            this.inputArray = Array.from(this.inputArray.filter(item => item.length > 0))
+            this.inputArray.push('')
+        },
+        deleteItem(index) {
+            this.inputArray.splice(index, 1)
+        }
+    }">
     <div @error($field->key.'.*') class="{{ $error() }}" @enderror>
         <template x-for="(item, index) in inputArray" :key="index">
             <div class="flex md:space-x-2 space-x-1">
-                <input x-model="inputArray[index]"
-                @foreach($options() as $key => $value) {{$key}}="{{$value}}" @endforeach
+                <input
+                    x-model="inputArray[index]"
+                    x-on:keydown.enter.prevent="addItem()"
+                    @foreach($options() as $key => $value) {{$key}}="{{$value}}" @endforeach
                 />
-                <button type="button" class="tf-repeater-delete-btn" x-on:click.prevent.prevent="inputArray.splice(index, 1)">
+                <button type="button" class="tf-repeater-delete-btn" x-on:click.prevent.prevent="deleteItem(index)">
                     <x-tall-svg :path="config('tall-forms.trash-icon')" class="tf-repeater-btn-size" />
                 </button>
             </div>
@@ -16,7 +27,7 @@
         </p>
         @enderror
     </div>
-    <button type="button" class="tf-repeater-add-button" x-on:click.prevent="inputArray = Array.from(inputArray.filter(item => item.length > 0)); inputArray.push('')" style="width:fit-content">
+    <button type="button" class="tf-repeater-add-button" x-on:click.prevent="addItem()" style="width:fit-content">
         <x-tall-svg :path="config('tall-forms.plus-icon')" class="tf-repeater-add-button-size" />
     </button>
 </div>
