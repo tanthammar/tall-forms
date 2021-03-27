c2e54e3cbb17c00b5fcf2b4750dfe7f523855151
tanthammar <tinahammar@gmail.com>
2021-03-27 17:48:41 +0100

    trix fix: catch style changes even if no text is changed.

--- resources/views/components/trix.blade.php
+++ resources/views/components/trix.blade.php
@@ -1,7 +1,20 @@
-<div class="w-full" x-data="{ trix: @entangle($field->key).defer }">
+<div class="w-full"
+     x-data="{
+        trix: @entangle($field->key).defer,
+        notFocused() { return document.activeElement !== this.$refs.trixInput },
+        setValue() {
+            {{-- replace content --}}
+            this.$refs.trixInput.editor.loadHTML(this.trix);
+            {{-- move cursor to the end--}}
+            this.$refs.trixInput.editor.setSelectedRange(this.$refs.trixInput.editor.getDocument().toString().length - 1);
+        },
+    }"
+     x-init="$watch('trix', () => notFocused() && setValue())"
+     x-on:trix-change.debounce.300ms="trix = $refs.trixInput.value"
+>
     <input value="{{ old($field->key) ?? $value }}" id="trix{{ md5($field->key) }}" name="trix{{ md5($field->key) }}" type="hidden" />
     <div wire:ignore x-on:trix-file-accept="return event.preventDefault()" class="no-upload">
-        <trix-editor x-model.debounce.500ms="trix" input="trix{{ md5($field->key) }}" {{ $attributes->merge(['class' => $errors->has($field->key) ? $error() : $class() ]) }}></trix-editor>
+        <trix-editor x-ref="trixInput" input="trix{{ md5($field->key) }}" {{ $attributes->merge(['class' => $errors->has($field->key) ? $error() : $class() ]) }}></trix-editor>
     </div>
 </div>
 @if($field->includeScript)
