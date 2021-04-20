7c7482935b49fd6b80c6519e9db2eecf6aff91dd
tanthammar <tinahammar@gmail.com>
2021-04-20 17:39:43 +0200

    shake style is moved to tall-theme.css, no need to push it in input-array.blade.php

--- resources/views/components/input-array.blade.php
+++ resources/views/components/input-array.blade.php
@@ -57,25 +57,3 @@
         <x-tall-svg :path="config('tall-forms.plus-icon')" class="tf-repeater-add-button-size" />
     </button>
 </div>
-@tfonce('styles:shake')
-<style>
-    .shake {
-        animation: shake 0.82s cubic-bezier(.36,.07,.19,.97) both;
-        transform: translate3d(0, 0, 0);
-    }
-    @keyframes shake {
-        10%, 90% {
-            transform: translate3d(-1px, 0, 0);
-        }
-        20%, 80% {
-            transform: translate3d(2px, 0, 0);
-        }
-        30%, 50%, 70% {
-            transform: translate3d(-4px, 0, 0);
-        }
-        40%, 60% {
-            transform: translate3d(4px, 0, 0);
-        }
-    }
-</style>
-@endtfonce
