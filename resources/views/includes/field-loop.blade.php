1bf035d47985970a8364622b4ef46fa8366a07bb
tanthammar <tinahammar@gmail.com>
2021-07-13 16:17:50 +0200

    removed obsolete ConfigAttr class from field-loop

--- resources/views/includes/field-loop.blade.php
+++ resources/views/includes/field-loop.blade.php
@@ -1,5 +1,5 @@
 {{-- used by views.includes.tabs and views.form --}}
-<x-tall-attr :attr="\Tanthammar\TallForms\ConfigAttr::key('fields-wrapper')">
+<x-tall-attr :attr="config('tall-forms.component-attributes.fields-wrapper', [])">
     @foreach($fields as $field)
         @if(filled($field))
             @include('tall-forms::includes.field-root')
