e5ac73afff73c8abd13a8aeb226942e0f6558beb
tanthammar <tinahammar@gmail.com>
2021-07-23 16:06:53 +0200

    Label for unique field id

--- resources/views/components/label-field-wrapper.blade.php
+++ resources/views/components/label-field-wrapper.blade.php
@@ -2,7 +2,7 @@
     {{-- label column --}}
     @if($field->show_label)
         <div class="{{ $labelWidth() }} {{ $field->labelWrapperClass }}">
-            <label for="{{ $field->key }}" class="{{ $field->labelClass }}">
+            <label for="{{ $field->getHtmlId($_instance->id) }}" class="{{ $field->labelClass }}">
                 {{$field->label}} <span class="tf-label-suffix">{{ $field->labelSuffix }}</span>
             </label>
             @if(!$field->afterLabelView && $field->afterLabel)
