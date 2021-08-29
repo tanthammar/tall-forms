bad75ec1113066a8ce4540169a0c6d53fa65a19e
tanthammar <tinahammar@gmail.com>
2021-08-29 23:26:34 +0200

    don't wrap with div in fields only.

--- resources/views/fields-only.blade.php
+++ resources/views/fields-only.blade.php
@@ -1,8 +1,6 @@
-<div class="tf-form">
-    @foreach($fields as $field)
-        @if(filled($field))
-            @php $field->setHtmlId($_instance->id); @endphp
-            @include('tall-forms::includes.field-root')
-        @endif
-    @endforeach
-</div>
+@foreach($fields as $field)
+    @if(filled($field))
+        @php $field->setHtmlId($_instance->id); @endphp
+        @include('tall-forms::includes.field-root')
+    @endif
+@endforeach
