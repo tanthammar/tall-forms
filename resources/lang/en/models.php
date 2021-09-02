411fc80cbd99f72fe1b71e51a9c852634b5b419f
tanthammar <tinahammar@gmail.com>
2021-09-02 13:11:51 +0200

    model translations

---
+++ resources/lang/en/models.php
@@ -0,0 +1,224 @@
+<?php
+
+return [
+    'permission' => [
+        'plural' => 'Permissions',
+        'singular' => 'Permission'
+    ],
+    'role' => [
+        'plural' => 'Roles',
+        'singular' => 'Role'
+    ],
+    'user' => [
+        'plural' => 'Users',
+        'singular' => 'User'
+    ],
+    'team' => [
+        'plural' => 'Teams',
+        'singular' => 'Team'
+    ],
+    'auditLog' => [
+        'plural' => 'Audit Logs',
+        'singular' => 'Audit Log'
+    ],
+    'faq' => [
+        'plural' => 'FAQs',
+        'singular' => 'FAQ'
+    ],
+    'question' => [
+        'plural' => 'Questions',
+        'singular' => 'Question'
+    ],
+    'status' => [
+        'plural' => 'Statuses',
+        'singular' => 'Status'
+    ],
+    'tag' => [
+        'plural' => 'Tags',
+        'singular' => 'Tag'
+    ],
+    'task' => [
+        'plural' => 'Tasks',
+        'singular' => 'Task'
+    ],
+    'calendar' => [
+        'plural' => 'Calendars',
+        'singular' => 'Calendar'
+    ],
+    'category' => [
+        'plural' => 'Categories',
+        'singular' => 'Category'
+    ],
+    'page' => [
+        'plural' => 'Pages',
+        'singular' => 'Page'
+    ],
+    'setting' => [
+        'plural' => 'Settings',
+        'singular' => 'Setting'
+    ],
+    'vat' => [
+        'plural' => 'Vat Rates',
+        'singular' => 'Vat Rate'
+    ],
+    'organizer' => [
+        'plural' => 'Organizers',
+        'singular' => 'Organizer'
+    ],
+    'location' => [
+        'plural' => 'Locations',
+        'singular' => 'Location'
+    ],
+    'locationPlan' => [
+        'plural' => 'Location Plans',
+        'singular' => 'Location Plan'
+    ],
+    'map' => [
+        'plural' => 'Maps',
+        'singular' => 'Map'
+    ],
+    'market' => [
+        'plural' => 'Markets',
+        'singular' => 'Market'
+    ],
+    'fair' => [
+        'plural' => 'Fairs',
+        'singular' => 'Fair'
+    ],
+    'festival' => [
+        'plural' => 'Festivals',
+        'singular' => 'Festival'
+    ],
+    'event' => [
+        'plural' => 'Events',
+        'singular' => 'Event'
+    ],
+    'customer' => [
+        'plural' => 'Customers',
+        'singular' => 'Customer'
+    ],
+    'client' => [
+        'plural' => 'Clients',
+        'singular' => 'Client'
+    ],
+    'exhibitor' => [
+        'plural' => 'Exhibitors',
+        'singular' => 'Exhibitor',
+    ],
+    'date' => [
+        'plural' => 'Dates',
+        'singular' => 'Date'
+    ],
+    'price' => [
+        'plural' => 'Prices',
+        'singular' => 'Price'
+    ],
+    'article' => [
+        'plural' => 'Articles',
+        'singular' => 'Article'
+    ],
+    'service' => [
+        'plural' => 'Services',
+        'singular' => 'Service'
+    ],
+    'rental' => [
+        'plural' => 'Rentals',
+        'singular' => 'Rental'
+    ],
+    'product' => [
+        'plural' => 'Products',
+        'singular' => 'Product'
+    ],
+    'term' => [
+        'plural' => 'Terms & Conditions',
+        'singular' => 'Term'
+    ],
+    'tab' => [
+        'plural' => 'Tabs',
+        'singular' => 'Tab'
+    ],
+    'booking' => [
+        'plural' => 'Bookings',
+        'singular' => 'Booking'
+    ],
+    'transaction' => [
+        'plural' => 'Transactions',
+        'singular' => 'Transaction'
+    ],
+    'invoice' => [
+        'plural' => 'Invoices',
+        'singular' => 'Invoice'
+    ],
+    'credit' => [
+        'plural' => 'Credits',
+        'singular' => 'Credit'
+    ],
+    'payment' => [
+        'plural' => 'Payments',
+        'singular' => 'Payment'
+    ],
+    'refund' => [
+        'plural' => 'Refunds',
+        'singular' => 'Refund'
+    ],
+    'orderRow' => [
+        'plural' => 'Order Lines',
+        'singular' => 'Order Line'
+    ],
+    'verification' => [
+        'plural' => 'Verifications',
+        'singular' => 'Verification'
+    ],
+    'verificationRow' => [
+        'plural' => 'Verification Rows',
+        'singular' => 'Verification Row'
+    ],
+    'attachement' => [
+        'plural' => 'Attachements',
+        'singular' => 'Attachement'
+    ],
+    'document' => [
+        'plural' => 'Documents',
+        'singular' => 'Document'
+    ],
+    'note' => [
+        'plural' => 'Notes',
+        'singular' => 'Note'
+    ],
+    'account' => [
+        'plural' => 'Accounts',
+        'singular' => 'Account'
+    ],
+    'help' => [
+        'plural' => 'Help',
+        'singular' => 'Help'
+    ],
+    'message' => [
+        'plural' => 'Messages',
+        'singular' => 'Message'
+    ],
+    'groupmail' => [
+        'plural' => 'Group Mails',
+        'singular' => 'Group Mail'
+    ],
+    'vipInvitation' => [
+        'plural' => 'Vip Invitations',
+        'singular' => 'Vip Invitation'
+    ],
+    'image' => [
+        'plural' => 'Images',
+        'singular' => 'Image'
+    ],
+    'media' => [
+        'plural' => 'Media',
+        'singular' => 'Media'
+    ],
+    'notification' => [
+        'plural' => 'Notifications',
+        'singular' => 'Notification'
+    ],
+    'subscription' => [
+        'plural' => 'Subscriptions',
+        'singular' => 'Subscription'
+    ],
+];
---
+++ resources/lang/sv/models.php
@@ -0,0 +1,224 @@
+<?php
+
+return [
+    'permission' => [
+            'plural' => 'Rättigheter',
+            'singular' => 'Rättighet',
+        ],
+    'role' => [
+            'plural' => 'Roller',
+            'singular' => 'Roll',
+        ],
+    'user' => [
+            'plural' => 'Användare',
+            'singular' => 'Användare',
+        ],
+    'team' => [
+            'plural' => 'Teams',
+            'singular' => 'Team',
+        ],
+    'auditLog' => [
+            'plural' => 'Audit Logs',
+            'singular' => 'Audit Log',
+        ],
+    'faq' => [
+            'plural' => 'FAQs',
+            'singular' => 'FAQ',
+        ],
+    'question' => [
+            'plural' => 'Frågor',
+            'singular' => 'Fråga',
+        ],
+    'status' => [
+            'plural' => 'Status',
+            'singular' => 'Status',
+        ],
+    'tag' => [
+            'plural' => 'Tags',
+            'singular' => 'Tag',
+        ],
+    'task' => [
+            'plural' => 'Uppgifter',
+            'singular' => 'Uppgift',
+        ],
+    'calendar' => [
+            'plural' => 'Kalendrar',
+            'singular' => 'Kalender',
+        ],
+    'category' => [
+            'plural' => 'Kategorier',
+            'singular' => 'Kategori',
+        ],
+    'page' => [
+            'plural' => 'Pages',
+            'singular' => 'Page',
+        ],
+    'setting' => [
+            'plural' => 'Inställningar',
+            'singular' => 'Inställning',
+        ],
+    'vat' => [
+            'plural' => 'Momssatser',
+            'singular' => 'Momssats',
+        ],
+    'organizer' => [
+            'plural' => 'Arrangörer',
+            'singular' => 'Arrangör',
+        ],
+    'location' => [
+            'plural' => 'Platser',
+            'singular' => 'Plats',
+        ],
+    'locationPlan' => [
+            'plural' => 'Planritningar',
+            'singular' => 'Planritning',
+        ],
+    'map' => [
+            'plural' => 'Kartor',
+            'singular' => 'Karta',
+        ],
+    'market' => [
+            'plural' => 'Marknader',
+            'singular' => 'Marknad',
+        ],
+    'fair' => [
+            'plural' => 'Mässor',
+            'singular' => 'Mässa',
+        ],
+    'festival' => [
+            'plural' => 'Festivaler',
+            'singular' => 'Festival',
+        ],
+    'event' => [
+            'plural' => 'Evenemang',
+            'singular' => 'Evenemang',
+        ],
+    'customer' => [
+            'plural' => 'Kunder',
+            'singular' => 'Kund',
+        ],
+    'client' => [
+        'plural' => 'Klienter',
+        'singular' => 'Klient',
+    ],
+    'exhibitor' => [
+        'plural' => 'Utställare',
+        'singular' => 'Utställare',
+    ],
+    'date' => [
+            'plural' => 'Datum',
+            'singular' => 'Datum',
+        ],
+    'price' => [
+            'plural' => 'Priss',
+            'singular' => 'Pris',
+        ],
+    'article' => [
+            'plural' => 'Artiklar',
+            'singular' => 'Artikel',
+        ],
+    'service' => [
+            'plural' => 'Tjänster',
+            'singular' => 'Tjänst',
+        ],
+    'rental' => [
+            'plural' => 'Uthyrningsartiklar',
+            'singular' => 'Uthyrningsartikel',
+        ],
+    'product' => [
+            'plural' => 'Produkter',
+            'singular' => 'Produkt',
+        ],
+    'term' => [
+            'plural' => 'Bokningsvillkor',
+            'singular' => 'Avtalspunkt',
+        ],
+    'tab' => [
+            'plural' => 'Flikar',
+            'singular' => 'Flik',
+        ],
+    'booking' => [
+            'plural' => 'Bokningar',
+            'singular' => 'Bokning',
+        ],
+    'transaction' => [
+            'plural' => 'Transactions',
+            'singular' => 'Transaction',
+        ],
+    'invoice' => [
+            'plural' => 'Fakturor',
+            'singular' => 'Faktura',
+        ],
+    'credit' => [
+            'plural' => 'Kreditnotor',
+            'singular' => 'Kreditnota',
+        ],
+    'payment' => [
+            'plural' => 'Inbetalningar',
+            'singular' => 'Inbetalning',
+        ],
+    'refund' => [
+            'plural' => 'Återbetalningar',
+            'singular' => 'Återbetalning',
+        ],
+    'orderRow' => [
+            'plural' => 'Orderrader',
+            'singular' => 'Orderrad',
+        ],
+    'verification' => [
+            'plural' => 'Verifikationer',
+            'singular' => 'Verifikation',
+        ],
+    'verificationRow' => [
+            'plural' => 'Konteringar',
+            'singular' => 'Kontering',
+        ],
+    'attachement' => [
+            'plural' => 'Bilagor',
+            'singular' => 'Bilaga',
+        ],
+    'document' => [
+            'plural' => 'Dokument',
+            'singular' => 'Dokument',
+        ],
+    'note' => [
+            'plural' => 'Anteckningar',
+            'singular' => 'Anteckning',
+        ],
+    'account' => [
+            'plural' => 'Konton',
+            'singular' => 'Konto',
+        ],
+    'help' => [
+            'plural' => 'Hjälp',
+            'singular' => 'Hjälp',
+        ],
+    'message' => [
+            'plural' => 'Meddelanden',
+            'singular' => 'Meddelande',
+        ],
+    'groupmail' => [
+            'plural' => 'Gruppmeddelanden',
+            'singular' => 'Gruppmeddelande',
+        ],
+    'vipInvitation' => [
+            'plural' => 'Vip Inbjudningar',
+            'singular' => 'Vip Inbjudan',
+        ],
+    'image' => [
+            'plural' => 'Bilder',
+            'singular' => 'Bild',
+        ],
+    'media' => [
+            'plural' => 'Media',
+            'singular' => 'Media',
+        ],
+    'notification' => [
+            'plural' => 'Notifieringar',
+            'singular' => 'Notifiering',
+        ],
+    'subscription' => [
+            'plural' => 'Prenumerationer',
+            'singular' => 'Prenumeration',
+        ],
+];
