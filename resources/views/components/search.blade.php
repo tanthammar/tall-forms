8885b85313e2367447d0732751a319261a2a37e4
tanthammar <tinahammar@gmail.com>
2021-09-10 17:48:38 +0200

    fix search entangle

--- resources/views/components/search.blade.php
+++ resources/views/components/search.blade.php
@@ -1,8 +1,8 @@
 <div x-data="{
     optionsVisible: false,
-    field: @this.entangle('{{$field->key}}'),
+    field: $wire.entangle('{{$field->key}}'),
     selected: null,
-    searchInput: @this.entangle('{{$field->searchKey}}')
+    searchInput: $wire.entangle('{{$field->searchKey}}')
     }"
     class="{{ $field->wrapperClass }}">
     <div class="relative">
@@ -17,7 +17,7 @@
                 {{ $attributes->except(array_keys($attr))->merge($attr)->merge(['class' => $errors->has($field->key) ? $field->errorClass : $field->class ]) }}
             />
             <div x-on:click.prevent="searchInput = ''; optionsVisible = false;" class="absolute inset-y-0 right-0 pr-3 flex items-center cursor-pointer">
-                <!-- Heroicon name: x -->
+                <!-- clear search field icon x -->
                 <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                     <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
                 </svg>
