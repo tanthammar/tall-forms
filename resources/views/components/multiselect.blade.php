3e1cf27f818a37277030cb220311f35af4e8012e
tanthammar <tinahammar@gmail.com>
2021-09-24 16:42:51 +0200

    wip bug conditional class

--- resources/views/components/multiselect.blade.php
+++ resources/views/components/multiselect.blade.php
@@ -1,40 +1,30 @@
 <div x-data="{
     open: false,
-    field: $wire.entangle('{{ $field->key }}'){{ $field->deferString }}
-    }" @if($field->wrapperClass) class="{{ $field->wrapperClass }}" @endif>
-    <div x-cloak
-         x-show="!field.length"
+    field: $wire.entangle('{{ $field->key }}'){{ $field->deferString }},
+    }" x-cloak @if($field->wrapperClass) class="{{ $field->wrapperClass }}" @endif>
+    <div x-show="!field.length"
          type="text"
          class="form-select my-1 cursor-pointer"
-         x-on:click="open = !open">
+         x-on:click.stop="open = !open">
         {{ $field->placeholder }}
     </div>
-    {{-- TODO investigate why the merge error class, below doesn't apply the class, then remove the wrapping div --}}
-    <div @class([
-            'border px-1 form-select',
-            $field->errorClass => $errors->has($field->key),
-            $field->class => !$errors->has($field->key)
-    ])>
-    <select x-cloak
-            x-show="open || field.length"
-            x-model="field"
-            x-on:click.outside.stop="open = false"
-            multiple
-            @if($field->disabled) disabled @endif
-            class="@error($field->key) tf-field-error @else form-select @enderror"
-            {{ $attributes->except([...array_keys($attr), 'disabled', 'multiple', 'class'])->whereDoesntStartWith('x-model')->merge($attr) }}>
-        @forelse($field->options as $value => $label)
-            <option class="p-2" wire:key="id{{ md5($field->id.$field->key.$value) }}" value="{{ $value }}">{{ $label }}</option>
-        @empty
-            <option class="p-2" value="" disabled>...</option>
-        @endforelse
-    </select>
-    </div>
-    <div>
-        @if ($errors->any())
-            @foreach ($errors->keys() as $message)
-                <div>{{ $message }}</div>
-            @endforeach
-        @endif
+    <div x-show="open || field.length" wire:key="wrap{{ md5($field->id.$field->key) }}">
+        <select x-model="field"
+                x-on:click.outside.stop="open = false"
+                multiple
+                @if($field->disabled) disabled @endif
+                {{ $attributes->except([...array_keys($attr), 'disabled', 'multiple', 'class'])
+                    ->whereDoesntStartWith('x-model')
+                    ->merge($attr)
+                    ->class([
+                        $field->errorClass => $errors->has($field->key),
+                        $field->class => !$errors->has($field->key)
+                ]) }}>
+            @forelse($field->options as $value => $label)
+                <option class="p-2" wire:key="id{{ md5($field->id.$field->key.$value) }}" value="{{ $value }}">{{ $label }}</option>
+            @empty
+                <option class="p-2" value="" disabled>...</option>
+            @endforelse
+        </select>
     </div>
 </div>
