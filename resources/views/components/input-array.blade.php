c0cc28dde902395b27bffc8053a9d044f7cc32a0
tanthammar <tinahammar@gmail.com>
2021-10-01 16:42:16 +0200

    add name and input id attributes to inputArray

--- resources/views/components/input-array.blade.php
+++ resources/views/components/input-array.blade.php
@@ -2,15 +2,19 @@
         maxItems: {{ $field->maxItems }},
         minItems: {{ $field->minItems }},
         items: $wire.entangle('{{ $field->key }}'){{ $field->deferString }},
+        fieldName: '{{ $field->name }}',
+        fieldKey: '{{ $field->key }}',
         inputs: $refs.inputs
     })" class="{{ $field->wrapperClass }}">
     <div @error($field->key.'.*') class="{{ $field->errorClass }}" @enderror>
         <fieldset x-ref="inputs" id="{{ $field->id }}" name="{{ $field->name }}">
-            <template x-for="(item, itemsIndex) in items" :key="itemsIndex">
+            <template x-for="(item, itemsIndex) in items" x-bind:key="itemsIndex">
                 <div class="flex md:space-x-2 space-x-1">
                     <input
                         @if($field->disabled) disabled @endif
                         x-model="items[itemsIndex]"
+                        x-bind:name="fieldName+itemsIndex"
+                        x-bind:id="fieldKey+itemsIndex"
                         x-on:keydown.enter.prevent="addItem()"
                         x-on:keydown.backspace="if(items[itemsIndex].length == 0) deleteItem(itemsIndex)"
                         {{ $attributes->except([...array_keys($attr), 'x-model', 'disabled'])->whereDoesntStartWith('x-model')->merge($attr) }}
@@ -38,6 +42,8 @@
             maxItems: config.maxItems,
             minItems: config.minItems,
             items: config.items,
+            fieldName: config.fieldName,
+            fieldKey: config.fieldKey,
             inputs: config.inputs,
             addItem() {
                 this.items = Array.from(this.items.filter(item => item.length > 0))
