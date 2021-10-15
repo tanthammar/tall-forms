5a2f164c24ab4dbc3095e0717890a1c6c301ebec
tanthammar <tinahammar@gmail.com>
2021-10-15 13:18:48 +0200

    remove $alert property in notify() trait

--- src/Traits/Notify.php
+++ src/Traits/Notify.php
@@ -2,57 +2,64 @@
 
 namespace Tanthammar\TallForms\Traits;
 
+use Illuminate\Validation\Rule;
+
 trait Notify
 {
     /*
     You can use this trait in both php or in frontend js
-    It either sets a LiveWire property that calls the notify method or
-    you can call the notify method directly.
-    example use in Alpine js to update the property
-    $wire.set('alert', {
-        type: 'danger',
-        message: Locale == 'sv' //if locale is shared to window
-        ? "Bilden du valt överskrider max tillåten dokument storlek. Se 'Max file size' ovanför bilden."
-        : "The image you uploaded exceeds max allowed file size, stated above the picture"
-    });
     exeample use in js to call the notify function
     $wire.call('notify', 'danger', 'Oh No!');
     */
-    public array $alert = [];
+
     protected bool $withSession = false;
 
+    //TODO find out why this is executed twice, until then remove it
+    /**
+     * example use in Alpine js to update the property
+        $wire.set('alert', {
+        preset: 'danger',
+        message: Locale == 'sv' //if locale is shared to window
+            ? "Bilden du valt överskrider max tillåten dokument storlek. Se 'Max file size' ovanför bilden."
+            : "The image you uploaded exceeds max allowed file size, stated above the picture"
+        });
+    public array $alert = [];
     public function updatedAlert()
     {
-        //TODO validate alert values
+        $this->validateOnly('alert', [
+            'alert.*' => Rule::in(['preset', 'message', 'bg', 'icon', 'iconcolor']),
+            'alert.preset' => ['nullable', Rule::in(['success', 'green', 'saved', 'check', 'warning', 'orange', 'happy', 'positive', 'sad', 'negative', 'danger', 'red', 'info', 'blue'])],
+            'alert.message' => 'nullable|alpha_dash|between:2,100',
+            'alert.bg' => 'nullable|alpha_dash|between:2,30',
+            'alert.icon' => ['nullable', Rule::in(['check', 'exclamation', 'happy', 'sad', 'warning', 'info'])],
+            'alert.iconcolor' => 'nullable|alpha_dash|between:2,30',
+        ]);
+
         $this->notify(
-            preset: data_get($this->alert, 'type', 'saved'),
+            preset: data_get($this->alert, 'preset', 'saved'),
             message: data_get($this->alert, 'message', trans('tf::form.alerts.updated-success')),
             bg: data_get($this->alert, 'bg'),
             icon: data_get($this->alert, 'icon'),
             iconcolor: data_get($this->alert, 'iconcolor')
         );
     }
+*/
 
     protected function withSession(): static
     {
-		$this->withSession = true;
-		return $this;
-	}
+        $this->withSession = true;
+        return $this;
+    }
 
     /**
      * Presets: 'info', 'success, 'warning', 'danger', 'happy', 'sad'
-     * @param string|null $preset
-     * @param string $message
-     * @param string $bg
-     * @param string $icon
-     * @param string $iconcolor
      */
     public function notify(
         null|string $preset = "saved",
-        string      $message = "",
-        string      $bg = 'tf-notify-bg-default',
-        string      $icon = "info",
-        string      $iconcolor = "text-white")
+        null|string $message = "",
+        null|string $bg = 'tf-notify-bg-default',
+        null|string $icon = "info",
+        null|string $iconcolor = "text-white")
     {
 
         [$bg, $icon, $iconcolor] = match ($preset) {
@@ -62,28 +69,28 @@ trait Notify
             'sad', 'negative'                    => ['bg-white', 'sad', 'text-red-600'],
             'danger', 'red'                      => ['tf-bg-danger', 'warning', $iconcolor],
             'info', 'blue'                       => ['tf-bg-info', 'info', $iconcolor],
-            default                              => [$bg, $icon, $iconcolor],
+            default => [$bg, $icon, $iconcolor],
         };
 
-        if($preset == 'saved') {
+        if ($preset == 'saved') {
             $message = $message ?: trans('tf::form.alerts.updated-success');
             $this->emitSelf('notify-saved');//x-on:notify-saved.window, flash trans('tf::form.saved') on the form submit button, buttons.root.blade.php
         }
 
         $payload = [
-			'bg'        => $bg,
-			'message'   => $message,
-            'icon'      => $icon,
+            'bg' => $bg,
+            'message' => $message,
+            'icon' => $icon,
             'iconcolor' => $iconcolor,
-		];
+        ];
 
-		if ($this->withSession) {
-			session()->flash('notify', $payload);
+        if ($this->withSession) {
+            session()->flash('notify', $payload);
             //reset withSession
             $this->withSession = false;
-			return;
-		}
+            return;
+        }
 
-		$this->dispatchBrowserEvent('notify', $payload);
+        $this->dispatchBrowserEvent('notify', $payload);
     }
 }
