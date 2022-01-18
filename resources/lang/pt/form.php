5b01085144f3017810b96589ef5641d7a439555c
Pedro Saraiva <pdro.saraiva@gmail.com>
2022-01-18 09:47:10 +0000

    add portuguese locales

---
+++ resources/lang/pt/form.php
@@ -0,0 +1,75 @@
+<?php
+
+return [
+	'actions' => 'Ações',
+	'add' => 'Adicionar',
+	'cancel' => 'Cancelar',
+	'create' => 'Criar',
+	'delete' => 'Apagar',
+	'edit' => 'Editar',
+	'new' => 'Novo',
+	'reset' => 'Reiniciar',
+	'save' => 'Guardar',
+	'saved' => 'Guardado',
+	'show' => 'Mostrar',
+	'swap' => 'Trocar',
+	'save-and-stay' => 'Guardar e continuar',
+	'save-go-back' => 'Guardar e voltar',
+
+	'cropper' => [
+		'select-file' => 'Selecionar ficheiro',
+		'drop-zone-help' => 'Arraste o ficheiro para esta área ou clique para selecionar',
+		'file-info' => 'PNG, JPG, GIF, TIFF, máx 1.5MB',
+	],
+
+	'file-upload' => [
+		'upload-file-error' => 'Um ou mais ficheiros enviados não corresponderam ao tipo de ficheiro permitido ou ao limite de tamanho. Consulte o texto de ajuda dos campos, revise os seus ficheiros e tente novamente.'
+	],
+
+	'select' => [
+		'placeholder' => 'Por favor selecione uma opção...',
+	],
+
+	'multiselect' => [
+		'placeholder' => 'Por favor favor selecione uma ou mais opções ...',
+		'help' => 'Pressione CTRL(Windows) ou CMD(Mac), para selecionar/desmarcar várias opções.',
+	],
+
+	'search' => [
+		'placeholder' => 'Procurar ...', //used for both Search and SearchList fields
+	],
+
+	'alerts' => [
+		'size-limit' => 'O ficheiro é muito grande.',
+		'one-at-the-time' => 'Por favor carregue apenas um ficheiro de cada vez.',
+		'max-attachments' => 'Nº máximo de anexos permitidos: ',
+		'mime' => 'Tipo de ficheiro inválido, escolha outro ficheiro.',
+		'updated-success' => 'Formulário guardado com sucesso',
+		'are-u-sure' => 'Tem a certeza?',
+		'tampering' => 'Atenção! Adulteração de valor não autorizada', //if someone tampers with read-only values in Livewire devtools or console
+	],
+
+	'spatie-tags' => [
+		'error-msg' => 'A tag deve ter entre 3 a 60 caracteres e consistir apenas em caracteres alfanuméricos.',
+		'placeholder' => 'Escrever para adicionar/procurar ...',
+		'help' => 'Pressione espaço para criar uma nova tag, ou selecione uma tag existente clicando nela.',
+	],
+
+	'tags' => [
+		'placeholder' => 'Escrever aqui …',
+		'help' => 'Pressione enter, virgula ou espaço, para adicionar uma tag',
+		'error-msg' => 'A tag deve ter entre 3 a 25 caracteres e consistir apenas em caracteres alfanuméricos.',
+	],
+
+	'tags-search' => [
+		'placeholder' => 'Escrever para adicionar/procurar ...',
+		'help' => 'Pressionar enter, virgula ou espaço, para adicionar uma nova tag, ou selecionar uma tag existente clicando nela.',
+		'error-msg' => 'A tag deve ter entre 3 a 25 caracteres e consistir apenas em caracteres alfanuméricos.',
+	],
+
+	'date-picker' => [
+		'placeholder' => 'Selecionar data...',
+		'save-btn' => 'GUARDAR',
+	],
+
+];
---
+++ resources/lang/pt/models.php
@@ -0,0 +1,240 @@
+<?php
+
+return [
+    'Permission' => [
+        'plural' => 'Permissions',
+        'singular' => 'Permission'
+    ],
+    'Role' => [
+        'plural' => 'Roles',
+        'singular' => 'Role'
+    ],
+    'User' => [
+        'plural' => 'Users',
+        'singular' => 'User'
+    ],
+    'Team' => [
+        'plural' => 'Teams',
+        'singular' => 'Team'
+    ],
+    'AuditLog' => [
+        'plural' => 'Audit Logs',
+        'singular' => 'Audit Log'
+    ],
+    'Faq' => [
+        'plural' => 'FAQs',
+        'singular' => 'FAQ'
+    ],
+    'Question' => [
+        'plural' => 'Questions',
+        'singular' => 'Question'
+    ],
+    'Status' => [
+        'plural' => 'Statuses',
+        'singular' => 'Status'
+    ],
+    'Tag' => [
+        'plural' => 'Tags',
+        'singular' => 'Tag'
+    ],
+    'Task' => [
+        'plural' => 'Tasks',
+        'singular' => 'Task'
+    ],
+    'Calendar' => [
+        'plural' => 'Calendars',
+        'singular' => 'Calendar'
+    ],
+    'Category' => [
+        'plural' => 'Categories',
+        'singular' => 'Category'
+    ],
+    'Page' => [
+        'plural' => 'Pages',
+        'singular' => 'Page'
+    ],
+    'Setting' => [
+        'plural' => 'Settings',
+        'singular' => 'Setting'
+    ],
+    'Vat' => [
+        'plural' => 'Vat Rates',
+        'singular' => 'Vat Rate'
+    ],
+    'Visitor' => [
+        'plural' => 'Visitors',
+        'singular' => 'Visitor'
+    ],
+    'Guest' => [
+        'plural' => 'Guests',
+        'singular' => 'Guest'
+    ],
+    'Organizer' => [
+        'plural' => 'Organizers',
+        'singular' => 'Organizer'
+    ],
+    'Location' => [
+        'plural' => 'Locations',
+        'singular' => 'Location'
+    ],
+    'LocationPlan' => [
+        'plural' => 'Location Plans',
+        'singular' => 'Location Plan'
+    ],
+    'Map' => [
+        'plural' => 'Maps',
+        'singular' => 'Map'
+    ],
+    'Market' => [
+        'plural' => 'Markets',
+        'singular' => 'Market'
+    ],
+    'Fair' => [
+        'plural' => 'Fairs',
+        'singular' => 'Fair'
+    ],
+    'Festival' => [
+        'plural' => 'Festivals',
+        'singular' => 'Festival'
+    ],
+    'Event' => [
+        'plural' => 'Events',
+        'singular' => 'Event'
+    ],
+    'Customer' => [
+        'plural' => 'Customers',
+        'singular' => 'Customer'
+    ],
+    'Client' => [
+        'plural' => 'Clients',
+        'singular' => 'Client'
+    ],
+    'Exhibitor' => [
+        'plural' => 'Exhibitors',
+        'singular' => 'Exhibitor',
+    ],
+    'Date' => [
+        'plural' => 'Dates',
+        'singular' => 'Date'
+    ],
+    'Price' => [
+        'plural' => 'Prices',
+        'singular' => 'Price'
+    ],
+    'Article' => [
+        'plural' => 'Articles',
+        'singular' => 'Article'
+    ],
+    'Service' => [
+        'plural' => 'Services',
+        'singular' => 'Service'
+    ],
+    'Rental' => [
+        'plural' => 'Rentals',
+        'singular' => 'Rental'
+    ],
+    'Product' => [
+        'plural' => 'Products',
+        'singular' => 'Product'
+    ],
+    'Term' => [
+        'plural' => 'Terms & Conditions',
+        'singular' => 'Term'
+    ],
+    'Tab' => [
+        'plural' => 'Tabs',
+        'singular' => 'Tab'
+    ],
+    'Booking' => [
+        'plural' => 'Bookings',
+        'singular' => 'Booking'
+    ],
+    'Transaction' => [
+        'plural' => 'Transactions',
+        'singular' => 'Transaction'
+    ],
+    'Invoice' => [
+        'plural' => 'Invoices',
+        'singular' => 'Invoice'
+    ],
+    'Credit' => [
+        'plural' => 'Credits',
+        'singular' => 'Credit'
+    ],
+    'Payment' => [
+        'plural' => 'Payments',
+        'singular' => 'Payment'
+    ],
+    'Refund' => [
+        'plural' => 'Refunds',
+        'singular' => 'Refund'
+    ],
+    'OrderRow' => [
+        'plural' => 'Order Lines',
+        'singular' => 'Order Line'
+    ],
+    'Verification' => [
+        'plural' => 'Verifications',
+        'singular' => 'Verification'
+    ],
+    'VerificationRow' => [
+        'plural' => 'Verification Rows',
+        'singular' => 'Verification Row'
+    ],
+    'Attachement' => [
+        'plural' => 'Attachements',
+        'singular' => 'Attachement'
+    ],
+    'Document' => [
+        'plural' => 'Documents',
+        'singular' => 'Document'
+    ],
+    'Note' => [
+        'plural' => 'Notes',
+        'singular' => 'Note'
+    ],
+    'Account' => [
+        'plural' => 'Accounts',
+        'singular' => 'Account'
+    ],
+    'Help' => [
+        'plural' => 'Help',
+        'singular' => 'Help'
+    ],
+    'Message' => [
+        'plural' => 'Messages',
+        'singular' => 'Message'
+    ],
+    'Mail' => [
+        'plural' => 'Mail',
+        'singular' => 'Mail',
+    ],
+    'Email' => [
+        'plural' => 'Email',
+        'singular' => 'Email',
+    ],
+    'Groupmail' => [
+        'plural' => 'Group Mails',
+        'singular' => 'Group Mail'
+    ],
+    'VipInvitation' => [
+        'plural' => 'Vip Invitations',
+        'singular' => 'Vip Invitation'
+    ],
+    'Image' => [
+        'plural' => 'Images',
+        'singular' => 'Image'
+    ],
+    'Media' => [
+        'plural' => 'Media',
+        'singular' => 'Media'
+    ],
+    'Notification' => [
+        'plural' => 'Notifications',
+        'singular' => 'Notification'
+    ],
+    'Subscription' => [
+        'plural' => 'Subscriptions',
+        'singular' => 'Subscription'
+    ],
+];
